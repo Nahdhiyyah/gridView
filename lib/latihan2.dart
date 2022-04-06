1410f57d22af40822e374e8961481531a51eb021
FlutLab.io <>
2022-04-06 15:26:07 +0000

    Autosave

--- lib/latihan2.dart
+++ lib/latihan2.dart
@@ -65,3 +65,143 @@ Widget _drawerItem({required IconData icon, required String text, required Gestu
     onTap: onTap,
   );
 }
+
+class music extends StatelessWidget {
+  final List<String> lagu = [
+    "Berisik",
+    "Without You",
+    "Up",
+    "Hati-hati Di jalan",
+    "Happy",
+    "Attention"
+  ];
+  final List<String> artis = [
+    "Dere",
+    "Avicii",
+    "Inna",
+    "Tulus",
+    "Skinnyfabs",
+    "Charlie Puth"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: lagu.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Icon(
+                Icons.play_arrow,
+                color: Colors.black,
+              ),
+              title: Text(lagu[index], style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 17)),
+              subtitle: Text(artis[index]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class movie extends StatelessWidget {
+  final List<String> foto = [
+    "assets/images/1.jpg",
+    "assets/images/2.jpg",
+    "assets/images/3.jpg",
+    "assets/images/4.jpg",
+    "assets/images/5.jpg",
+    "assets/images/6.jpg"
+  ];
+  final List<String> judul = [
+    "Berisik",
+    "Without You",
+    "Up",
+    "Hati-hati Di Jalan",
+    "Happy",
+    "Attention"
+  ];
+
+  final List<String> artis = [
+    "Dere",
+    "Avicii",
+    "Inna",
+    "Tulus",
+    "Skinnyfabs",
+    "Charlie Puth"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: foto.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Image.asset(foto[index], width: 50, height: 50, fit: BoxFit.cover),
+              title: Text(judul[index], style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 17)),
+              subtitle: Text(artis[index]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class favorit extends StatelessWidget {
+  final List<String> gambar = [
+    "assets/images/4.jpg",
+    "assets/images/5.jpg",
+    "assets/images/6.jpg"
+  ];
+
+  final List<String> judul = [
+    "Hati-hati Di Jalan",
+    "Happy",
+    "Attention"
+  ];
+
+  final List<String> artis = [
+    "Tulus",
+    "Skinnyfabs",
+    "Charlie Puth"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: new Container(
+            child: ListView(
+      children: [
+        ListTile(
+          leading: Icon(Icons.play_arrow),
+          title: Text('Berisik', style: TextStyle(fontSize: 17)),
+        ),
+        ListTile(
+          leading: Icon(Icons.play_arrow),
+          title: Text('Without You', style: TextStyle(fontSize: 17)),
+        ),
+        ListTile(
+          leading: Icon(Icons.movie),
+          title: Text('Up', style: TextStyle(fontSize: 17)),
+        ),
+        ListTile(
+          leading: Icon(Icons.movie),
+          title: Text('Hati-hati Di Jalan', style: TextStyle(fontSize: 17)),
+        ),
+        ListTile(
+          leading: Icon(Icons.play_arrow),
+          title: Text('Happy', style: TextStyle(fontSize: 17)),
+        ),
+        ListTile(
+          leading: Icon(Icons.movie),
+          title: Text('Attention', style: TextStyle(fontSize: 17)),
+        ),
+      ],
+    )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,26 @@ void main() {
 class BelajarNavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Belajar Drawer Nav"),
-      ),
-      drawer: DrawerWidget(),
-      //endDrawer: DrawerWidget(),
-      body: Center(child: Text('Belajar Navigation Drawer di Flutter', style: TextStyle(fontSize: 20))),
-    );
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Nadia's List"),
+              bottom: TabBar(
+                tabs: [
+                  new Tab(icon: Icon(Icons.audiotrack), text: "My Music"),
+                  new Tab(icon: Icon(Icons.movie), text: "My Movie"),
+                  new Tab(icon: Icon(Icons.favorite), text: "Favorite")
+                ],
+              ),
+            ),
+            drawer: DrawerWidget(),
+            body: TabBarView(
+              children: <Widget>[
+                new music(),
+                new movie(),
+                new favorit()
+              ],
+            )));
   }
 }
