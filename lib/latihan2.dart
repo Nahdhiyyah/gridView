46b655bc375a9dc989e29e28c33e30f355582937
FlutLab.io <>
2022-04-06 04:16:42 +0000

    Autosave

--- lib/latihan2.dart
+++ lib/latihan2.dart
@@ -1,45 +1,67 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
+class DrawerWidget extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BelajarAppBar(),
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          _drawerHeader(),
+          _drawerItem(icon: Icons.folder, text: 'My Files', onTap: () => print('Tap My Files')),
+          _drawerItem(icon: Icons.group, text: 'Shared with me', onTap: () => print('Tap Shared menu')),
+          _drawerItem(icon: Icons.access_time, text: 'Recent', onTap: () => print('Tap Recent menu')),
+          _drawerItem(icon: Icons.delete, text: 'Trash', onTap: () => print('Tap Trash menu')),
+          Divider(height: 25, thickness: 1),
+          Padding(
+            padding: const EdgeInsets.only(left: 20.0, top: 10, bottom: 10),
+            child: Text("Labels",
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                )),
+          ),
+          _drawerItem(icon: Icons.bookmark, text: 'Family', onTap: () => print('Tap Family menu')),
+        ],
+      ),
     );
   }
 }
 
-class BelajarAppBar extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: NestedScrollView(
-            headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-              return <Widget>[
-                SliverAppBar(
-                    expandedHeight: 200.0,
-                    floating: false,
-                    pinned: true,
-                    flexibleSpace: FlexibleSpaceBar(
-                        centerTitle: true,
-                        title: Text("Belajar Sliver App Bar",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20.0,
-                            )),
-                        background: Image(
-                          image: AssetImage('assets/images/1.jpg'),
-                          fit: BoxFit.cover,
-                        )))
-              ];
-            },
-            body: Center(
-              child: Text("Gambar Kiyowo"),
-            )));
-  }
+Widget _drawerHeader() {
+  return UserAccountsDrawerHeader(
+    currentAccountPicture: ClipOval(
+      child: Image(image: AssetImage('assets/images/3.jpeg'), fit: BoxFit.cover),
+    ),
+    otherAccountsPictures: [
+      ClipOval(
+        child: Image(image: AssetImage('assets/images/4.jpg'), fit: BoxFit.cover),
+      ),
+      ClipOval(
+        child: Image(image: AssetImage('assets/images/5.jpeg'), fit: BoxFit.cover),
+      )
+    ],
+    accountName: Text('Belajar Flutter'),
+    accountEmail: Text('hallo@belajarflutter.com'),
+  );
+}
+
+Widget _drawerItem({required IconData icon, required String text, required GestureTapCallback onTap}) {
+  return ListTile(
+    title: Row(
+      children: <Widget>[
+        Icon(icon),
+        Padding(
+          padding: EdgeInsets.only(left: 25.0),
+          child: Text(
+            text,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+    onTap: onTap,
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,23 @@
 import 'package:flutter/material.dart';
+import 'latihan2.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: "BelajarFlutter.com",
+    home: BelajarNavigationDrawer(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class BelajarNavigationDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ContohDynamicAppBar(),
-    );
-  }
-}
-
-class ContohDynamicAppBar extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Material(
-        child: CustomScrollView(
-          slivers: [
-            SliverPersistentHeader(
-              delegate: MySliverAppBar(expandedHeight: 200),
-              pinned: true,
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (_, index) => ListTile(
-                  title: Text("Song $index"),
-                ),
-              ),
-            )
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar Drawer Nav"),
       ),
+      drawer: DrawerWidget(),
+      endDrawer: DrawerWidget(),
+      body: Center(child: Text('Belajar Navigation Drawer di Flutter', style: TextStyle(fontSize: 20))),
     );
   }
-}
-
-class MySliverAppBar extends SliverPersistentHeaderDelegate {
-  final double expandedHeight;
-
-  MySliverAppBar({@required this.expandedHeight = 0});
-
-  @override
-  Widget build(BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Stack(
-      fit: StackFit.expand,
-      overflow: Overflow.visible,
-      children: [
-        Image(
-          image: AssetImage('assets/images/1.jpg'),
-          fit: BoxFit.cover,
-        ),
-        Center(
-          child: Opacity(
-            opacity: shrinkOffset / expandedHeight,
-            child: Text(
-              "BabyMetal di BelajarFlutter",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
-                fontSize: 20,
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          top: expandedHeight / 2 - shrinkOffset,
-          left: MediaQuery.of(context).size.width / 4,
-          child: Opacity(
-            opacity: (1 - shrinkOffset / expandedHeight),
-            child: ClipOval(
-              child: Image(
-                width: MediaQuery.of(context).size.width / 2,
-                image: AssetImage('assets/images/2.jpg'),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  @override
-  double get maxExtent => expandedHeight;
-
-  @override
-  double get minExtent => kToolbarHeight;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
 }
