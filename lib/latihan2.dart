543dc22af779d8630482aad8901835faa470e621
FlutLab.io <>
2022-04-06 17:40:13 +0000

    Autosave

--- lib/latihan2.dart
+++ lib/latihan2.dart
@@ -48,7 +48,7 @@ class DrawerWidget extends StatelessWidget {
 
 Widget _drawerHeader() {
   return UserAccountsDrawerHeader(
-    decoration: BoxDecoration(color: Colors.pink),
+    decoration: BoxDecoration(color: Colors.pink[200]),
     currentAccountPicture: ClipOval(
       child: Image(image: AssetImage('assets/images/1.jpg'), fit: BoxFit.cover),
     ),
